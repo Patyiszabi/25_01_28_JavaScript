09c0624e875a29129e120e614f3531db71f1f63f
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-01-28 09:49:21 +0100

    .

---
+++ console_log_tipusok.js
@@ -0,0 +1,15 @@
+console.log("Ez egy normál üzenet");
+console.warn("Ez egy figyelmeztetés");
+console.error("Ez egy hibaüzenet");
+console.info("Ez egy információ");
+
+console.table(
+    [
+        {név: "Anna", életkor: 25},
+        {név: "Béla", életkor: 30},
+        {név: "Csaba", életkor: 35}
+        
+    ]
+);
+
+
