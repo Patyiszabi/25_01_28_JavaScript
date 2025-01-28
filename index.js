00544642ccd9d52d9590266467fda77ab27355c0
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-01-28 09:25:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello World!");
 console.log("Első Javasccript kód");
 
 function kep_beillesztese() {
-    document.getElementById(" kep_helye").innerHTML = "<img src='https://img.freepik.com/free-photo/smiling-handsome-redhead-man-showing-thumbs-up-approval-like-agree-praise-recommend-promotional-offer-satisfied-by-something-standing-white-wall_176420-38470.jpg' alt='Trulli' width='500' height='333'>";
+    document.getElementById(" kep_helye").innerHTML = "<img src='https://m.blog.hu/ma/maczoly/image/photo.jpg' alt='Trulli' width='500' height='333'>";
 }
 
 function kep_torlese() {
