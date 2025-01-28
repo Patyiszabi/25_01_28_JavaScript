f6ebb83b29ea014b62ea58d8141fe1b7579f9968
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-01-28 09:22:21 +0100

    .

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log("Első Javasccript kód");
 function kep_beillesztese() {
     document.getElementById(" kep_helye").innerHTML = "<img src='https://img.freepik.com/free-photo/smiling-handsome-redhead-man-showing-thumbs-up-approval-like-agree-praise-recommend-promotional-offer-satisfied-by-something-standing-white-wall_176420-38470.jpg' alt='Trulli' width='500' height='333'>";
 }
+
+function kep_torlese() {
+    document.getElementById(" kep_helye").innerHTML = "";
+}
